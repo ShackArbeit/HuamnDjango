8788b8b62cf6a8a087a08d8febc5c8d19e31c3f7
Shack <g0972222165@gmail.com>
2024-06-02 10:15:08 +0800

    Add HoemPage

--- homePage/admin.py
+++ homePage/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import BookingProcess,ServiceItems
 
-# Register your models here.
+
+
+class Process_Admin(admin.ModelAdmin):
+      list_display=('id','porcess_title','process_content','process_icon')
+
+
+class Service_Admin(admin.ModelAdmin):
+      list_display=('id','service_title','service_content')
+
+admin.site.register(BookingProcess,Process_Admin)
+admin.site.register(ServiceItems,Service_Admin)
--- homePage/models.py
+++ homePage/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class BookingProcess(models.Model):
+      porcess_title=models.CharField(max_length=50,blank=True,verbose_name='預約標題')
+      process_content=models.TextField(max_length=800,blank=True,verbose_name='預約內容')
+      process_icon=models.URLField(blank=True,verbose_name='預約圖示')
+      class Meta:
+            verbose_name = '預約流程'
+            verbose_name_plural = '預約流程'
+
+      def __str__(self):
+            return self.porcess_title
+
+class ServiceItems(models.Model):
+      service_title=models.CharField(max_length=50,blank=True,verbose_name='服務標題')
+      service_content=models.TextField(max_length=800,blank=True,verbose_name='預約內容')
+      class Meta:
+            verbose_name = '服務項目'
+            verbose_name_plural = '服務項目'
+
+      def __str__(self):
+            return self.service_title
+
