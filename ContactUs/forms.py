2e601374f06b35c79f9474dd4f20d800f3f3c9ec
Shack <g0972222165@gmail.com>
2024-08-05 11:18:00 +0800

    Add Correct Form

--- ContactUs/forms.py
+++ ContactUs/forms.py
@@ -4,4 +4,6 @@ class ContactForm(forms.Form):
     user_name = forms.CharField(label='您的姓名', max_length=50, initial='')
     user_book = forms.BooleanField(label='是否第一次預約', required=False)
     user_email = forms.EmailField(label='電子信箱')
-    user_message = forms.CharField(label='您的意見', widget=forms.Textarea)
+    user_line_id = forms.CharField(label='您的 Line ID', max_length=50) 
+    booking_description = forms.CharField(label='預約描述', widget=forms.Textarea)  
+    booking_date = forms.CharField(label='預約日期')  
--- ContactUs/views.py
+++ ContactUs/views.py
@@ -1,36 +1,53 @@
 from django.shortcuts import render
-
 from django.core.mail import EmailMessage
+from .forms import ContactForm
 
 
-def Contact(request):
-    message=''
-    if request.method=='POST':
-         user_name = request.POST.get('user_name')
-         user_check = '是' if 'user_check' in request.POST else '否'
-         user_email = request.POST.get('user_email')
-         user_message = request.POST.get('user_message')
+def contact(request):
+    message = ''
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['user_name']
+            user_book = '是' if form.cleaned_data['user_book'] else '否'
+            user_email = form.cleaned_data['user_email']
+            user_line_id = form.cleaned_data['user_line_id']
+            booking_description = form.cleaned_data['booking_description']
+            booking_date = form.cleaned_data['booking_date']
 
-         if user_name and user_email and user_message:
-               mail_body = f'''
+            mail_body = f'''
                 回饋者姓名：{user_name}
-                是否第一次預約：{user_check}
+                是否第一次預約：{user_book}
                 電子郵件：{user_email}
-                反應意見:
-                {user_message}
-                '''
-               
-               email = EmailMessage(
-                '來自【人類圖遇見八字】的回饋意見', 
-                mail_body, 
+                Line ID：{user_line_id}
+                預約描述：{booking_description}
+                預約日期：{booking_date}
+            '''
+            
+            email = EmailMessage(
+                '來自【人類圖遇見八字】的回饋意見',
+                mail_body,
                 user_email,
                 ['g0972222165@gmail.com']
-              )
-               try:
+            )
+            try:
                 email.send()
                 message = "感謝您的來信，我們會儘速處理您的寶貴意見。"
-               except Exception as e:
+            except Exception as e:
                 message = f"郵件發送失敗: {e}"
-         else:
+        else:
             message = "請檢查您輸入的資訊是否正確！"
-    return render(request, 'ContactForm.html', {'message': message})
+    else:
+        booking_description = request.GET.get('booking_description', '')
+        booking_date = request.GET.get('booking_date', '')
+        form = ContactForm(initial={
+            'booking_description': booking_description,
+            'booking_date': booking_date,
+        })
+
+    return render(request, 'ContactForm.html', {
+        'form': form,
+        'message': message,
+        'booking_description': booking_description,
+        'booking_date': booking_date,
+    })
